c46591f429fe27a518320e7a488b45528f12639c
Mohammad Takbiri <mohammad_online2013@yahoo.com>
2020-06-11 14:53:59 +0430

    Initial Commit

---
+++ MohammadTakbiri-CalculateDistance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MohammadTakbiri-CalculateDistance
+//
+//  Created by mohammad takbiri on 6/11/20.
+//  Copyright © 2020 Mohammad Takbiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
